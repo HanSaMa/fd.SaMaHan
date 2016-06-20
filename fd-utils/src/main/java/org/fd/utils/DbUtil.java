ee87c1cd7ea87c79615ce2abef1b8ce679ccee98
SaMa.Han <SaMa.Han@10.73.115.23>
2016-06-20 18:16:49 +0800

    UTILS包

---
+++ fd-utils/src/main/java/org/fd/utils/DbUtil.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package org.fd.utils;
+
+/**
+ * 
+ * <pre>
+ * 数据库连接工具类。
+ * </pre>
+ * 
+ * @author SaMa.Han guoqiang.han@foxmail.com
+ * @version 1.00.00
+ * 
+ *          <pre>
+ * 修改记录
+ *    修改后版本:     修改人：  	修改日期:     修改内容:
+ * </pre>
+ */
+public class DbUtil {
+	
+	
+}
