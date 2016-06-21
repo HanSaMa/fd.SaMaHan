f8bad372390c1a234bb31a5c1a8c021a024a15d0
SaMa.Han <SaMa.Han@10.73.162.28>
2016-06-21 11:04:56 +0800

    dbutil及相关constans

---
+++ fd-utils/src/main/java/org/fd/constants/JavaConstants.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package org.fd.constants;
+
+/**
+ * 
+ * <pre>
+ * 数据库连接工具类。
+ * </pre>
+ * 
+ * @author SaMa.Han guoqiang.han@foxmail.com
+ * @version 1.00.00
+ * 
+ *          <pre>
+ * 修改记录
+ *    修改后版本:     修改人：  	修改日期:     修改内容:
+ * </pre>
+ */
+public class JavaConstants {
+	public static final String SHORT = "short";
+	public static final String SHORT_TYPE = "java.lang.Short";
+	public static final String INTEGER = "int";
+	public static final String INTEGER_TYPE = "java.lang.Integer";
+	public static final String LONG = "long";
+	public static final String LONG_TYPE = "java.lang.Long";
+	public static final String DOUBLE = "double";
+	public static final String DOUBLE_TYPE = "java.lang.Double";
+	public static final String FLOAT = "float";
+	public static final String FLOAT_TYPE = "java.lang.Float";
+	public static final String BYTE = "byte";
+	public static final String BYTE_TYPE = "java.lang.Byte";
+	public static final String BOOLEAN = "boolean";
+	public static final String BOOLEAN_TYPE = "java.lang.Boolean";
+	public static final String STRING_TYPE = "java.lang.String";
+	public static final String DATE_TYPE = "java.util.Date";
+}
