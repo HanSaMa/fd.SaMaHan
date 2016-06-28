4f8d03f36324f85ab7e85eb84d86e5c83a5c8ae2
SaMa.Han <SaMa.Han@10.73.186.61>
2016-06-28 10:41:48 +0800

    添加生成GRID

--- fd-utils/src/main/java/com/fd/utils/midea/DbToX5Util.java
+++ fd-utils/src/main/java/com/fd/utils/midea/DbToX5Util.java
@@ -20,8 +20,8 @@ import com.fd.utils.StringUtil;
  *    修改后版本:     修改人：  	修改日期:     修改内容:
  * </pre>
  */
-public class DbToX5DataUtil {
-	private DbToX5DataUtil() {
+public class DbToX5Util {
+	private DbToX5Util() {
 	}
 
 	public static void printX5Data(String tables) throws Exception {
@@ -49,6 +49,35 @@ public class DbToX5DataUtil {
 			}
 			main.append(clmns);
 			main.append("</div>\n");
+			main.append("<!------------------------------------------------>\n");
+			System.out.println(main.toString());
+		}
+	}
+	
+	public static void printX5Grid(String tables) throws Exception {
+		String[] tablesArr = tables.split(StringUtil.SPLIT);
+
+		for (String table : tablesArr) {
+			table = table.trim();
+			System.out.println(table);
+			StringBuffer main = new StringBuffer();
+			String propertyName = StringUtil.underlineToCamel(table);
+			main.append("<div component=\"$UI/system/components/justep/grid/grid\" hiddenCaptionbar=\"true\"");
+			main.append(" altRows=\"true\" class=\"x-grid-no-bordered\" data=\"").append(propertyName).append("Data\"");
+			main.append(" xid=\"").append(propertyName).append("Gd\" height=\"auto\"> \n\t<columns>\n");
+
+			String sql = DbUtil.getQueryTableColumnsSQL(table.toUpperCase());
+			List<TableColumns> columns = DbUtil.queryPlural(TableColumns.class, sql);
+			
+			for (TableColumns column : columns) {
+				String columnName = StringUtil.underlineToCamel(column.getColumnName().toLowerCase());
+				
+				if(null != column.getDescription() && !column.getDescription().isEmpty()){
+					main.append("\t\t<column name=\"").append(columnName).append("\"/>\n");
+				}
+			}
+			main.append("\t\t<column name=\"delClmn\" label=\"删除\"/>\n");
+			main.append("\t</columns>\n</div>\n");
 			main.append("<!------------------------------------------------>\n");
 			System.out.println(main.toString());
 		}
